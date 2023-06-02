9db8b7872fbe15624f40ca5dc4116859b3a20cbd
bikendra-kc <kcbikey7@gmail.com>
2023-06-02 11:56:02 +0545

    app_theme pushed

---
+++ lib/theme/app_color_constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColorConstant {
+  AppColorConstant._();
+
+  static const Color primaryColor = Colors.black;
+  static const Color appBarColor = Colors.black;
+}
