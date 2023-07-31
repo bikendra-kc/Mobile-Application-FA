f3dd2c7e2ffa47a539aea6b7b2c9be3b8a0a3112
bikendra-kc <kcbikey7@gmail.com>
2023-07-31 21:10:44 +0545

    new file added

--- lib/view/dashboard.dart
+++ lib/view/dashboard.dart
@@ -3,5 +3,150 @@ import 'package:flutter/material.dart';
 class DashboardView extends StatelessWidget {
   const DashboardView({super.key});
 
- 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              padding: const EdgeInsets.all(16),
+              child: DataTable(
+                columns: const <DataColumn>[
+                  DataColumn(
+                    label: Text(
+                      'Crypto',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  DataColumn(
+                    label: Text(
+                      'Price',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+                rows: const <DataRow>[
+                  DataRow(
+                    cells: <DataCell>[
+                      DataCell(Text('Bitcoin')),
+                      DataCell(Text('3000')),
+                    ],
+                  ),
+                  DataRow(
+                    cells: <DataCell>[
+                      DataCell(Text('Dodgecoin')),
+                      DataCell(Text('0.01')),
+                    ],
+                  ),
+                  DataRow(
+                    cells: <DataCell>[
+                      DataCell(Text('Ethereum')),
+                      DataCell(Text('4000')),
+                    ],
+                  ),
+                  DataRow(
+                    cells: <DataCell>[
+                      DataCell(Text('Litecoin')),
+                      DataCell(Text('173')),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                MediaQueryButton(
+                  label: 'Portfolio',
+                  icon: Icons.pie_chart,
+                  color: Colors.blue,
+                ),
+                MediaQueryButton(
+                  label: 'Notes',
+                  icon: Icons.note,
+                  color: Colors.orange,
+                ),
+              ],
+            ),
+          ),
+          const Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                MediaQueryButton(
+                  label: 'News',
+                  icon: Icons.newspaper,
+                  color: Colors.green,
+                ),
+                MediaQueryButton(
+                  label: 'Profile',
+                  icon: Icons.person,
+                  color: Colors.purple,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MediaQueryButton extends StatelessWidget {
+  final String label;
+  final IconData icon;
+  final Color color;
+
+  const MediaQueryButton({super.key, 
+    required this.label,
+    required this.icon,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+      onPressed: () {
+        // Handle button tap
+      },
+      icon: Icon(
+        icon,
+        size: 30,
+      ),
+      label: Text(
+        label,
+        style: const TextStyle(fontSize: 18),
+      ),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        padding: const EdgeInsets.all(16),
+        minimumSize: const Size(150, 60),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        elevation: 3,
+        shadowColor: Colors.grey[400],
+      ),
+    );
+  }
 }
